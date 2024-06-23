2fdda7140ea014ab8f8e8559b14bc3a63dc0a469
VermaAyush2k4 <117018341+VermaAyush2k4@users.noreply.github.com>
2024-06-23 17:17:02 +0530

    Add files via upload

---
+++ letter-p/image-puzzle.js
@@ -0,0 +1,74 @@
+var imagePuzzle = {
+    startGame: function (images, gridSize) {
+        this.setImage(images, gridSize);
+        helper.doc('playPanel').style.display = 'block';
+        helper.shuffle('sortable');
+    },
+    setImage: function (images, gridSize = 4) {
+        var percentage = 100 / (gridSize - 1);
+        var image = images[Math.floor(Math.random() * images.length)];
+        helper.doc('imgTitle').innerHTML = image.title;
+        helper.doc('actualImage').setAttribute('src', image.src);
+        helper.doc('sortable').innerHTML = '';
+        for (var i = 0; i < gridSize * gridSize; i++) {
+            var xpos = (percentage * (i % gridSize)) + '%';
+            var ypos = (percentage * Math.floor(i / gridSize)) + '%';
+
+            let li = document.createElement('li');
+            li.id = i;
+            li.setAttribute('data-value', i);
+            li.style.backgroundImage = 'url(' + image.src + ')';
+            li.style.backgroundSize = (gridSize * 100) + '%';
+            li.style.backgroundPosition = xpos + ' ' + ypos;
+            li.style.width = 370 / gridSize + 'px';
+            li.style.height = 370 / gridSize + 'px';
+
+            li.setAttribute('draggable', 'true');
+            li.ondragstart = (event) => event.dataTransfer.setData('data', event.target.id);
+            li.ondragover = (event) => event.preventDefault();
+            li.ondrop = (event) => {
+                let origin = helper.doc(event.dataTransfer.getData('data'));
+                let dest = helper.doc(event.target.id);
+                let p = dest.parentNode;
+
+                if (origin && dest && p) {
+                    let temp = dest.nextSibling;
+                    let x_diff = origin.offsetLeft-dest.offsetLeft;
+                    let y_diff = origin.offsetTop-dest.offsetTop;
+
+                    if(y_diff == 0 && x_diff >0){
+                        //LEFT SWAP
+                        p.insertBefore(origin, dest);
+                        p.insertBefore(temp, origin);
+                    }
+                    else{
+                        p.insertBefore(dest, origin);
+                        p.insertBefore(origin, temp);
+                    }
+
+                    let vals = Array.from(helper.doc('sortable').children).map(x => x.id);
+                    if (isSorted(vals)) {
+                        helper.doc('playPanel').style.display = 'none';
+                        helper.doc('gameOver').style.display = 'block';
+                    }
+                }
+            };
+            li.setAttribute('dragstart', 'true');
+            helper.doc('sortable').appendChild(li);
+        }
+        helper.shuffle('sortable');
+    }
+};
+
+isSorted = (arr) => arr.every((elem, index) => { return elem == index; });
+
+var helper = {
+    doc: (id) => document.getElementById(id) || document.createElement("div"),
+
+    shuffle: (id) => {
+        var ul = document.getElementById(id);
+        for (var i = ul.children.length; i >= 0; i--) {
+            ul.appendChild(ul.children[Math.random() * i | 0]);
+        }
+    }
+}
